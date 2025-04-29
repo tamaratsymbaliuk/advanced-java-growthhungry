8790fb6171c5af515225577ee6c2ec3db42db75c
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-04-29 18:21:36 -0500

    Parking Lot Simulation using Semaphore practice

---
+++ src/week-10/homework/ParkingLotSimulation.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class ParkingLotSimulation {
+    public static void main(String[] args) {
+        ParkingLot parkingLot = new ParkingLot(5);
+
+        // Simulating 10 cars trying to park
+        for (int i = 1; i <= 10; i++) {
+            String carName = "Car-" + i;
+            new Thread(() -> parkingLot.parkCar(carName)).start();
+        }
+    } 
+}
