bedb9b6338c0797619f6f0cc549042edad909077
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-04-29 18:29:14 -0500

    Thread safe counter practice using ReentrantLock

--- src/week-10/homework/SafeCounter.java
+++ src/week-10/homework/SafeCounter.java
@@ -3,28 +3,38 @@ package homework;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SafeCounter {
-    private int count;
+    private int count = 0;
     private final ReentrantLock lock = new ReentrantLock();
 
-    public void increment(int count) {
+    public void increment() {
         lock.lock();
         try {
-            count += count;
+            count++;
             System.out.println(Thread.currentThread().getName() + " successfully incremented. Current count: " + count);
         } finally {
             lock.unlock();
         }
     }
 
-    public void decrement(int count) {
+    public void decrement() {
         lock.lock();
         try {
-            count -= count;
+            count--;
             System.out.println(Thread.currentThread().getName() + " successfully descremented. Current count: " + count);
         } finally {
             lock.unlock();
         }
     }
+    // Method to get the current count value
+    public int getValue() {
+        lock.lock(); // Acquire the lock
+        try {
+            return count;
+        } finally {
+            lock.unlock(); // Release the lock
+        }
+    }
 }
+
     
 
