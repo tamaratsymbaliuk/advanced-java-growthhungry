7de2db07eacb7420ed27cf3fd994293e978f74d5
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-04-30 20:21:59 -0500

    Employee class

---
+++ src/week-11/Employee.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    
+    @Override
+    public String toString() {
+        return "Employee{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", salary=" + salary +
+        '}';
+    }
+}
